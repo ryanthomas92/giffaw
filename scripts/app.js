db9d6fffc6f6f9805be2c3ca7b0aba9bdcc16fb4
Ryan_Thomas <rthoma11@villanova.edu>
2016-12-06 16:50:57 -0800

    AJAX code to display pics

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,24 @@
+console.log("JS is working")
 $(document).on("ready", function(){
-
+  $('form').on("submit", searchReq);
 });
+function searchReq(event){
+  console.log("searchReq function");
+  event.preventDefault();
+  $.ajax({
+    method: "GET",
+    url: "http://api.giphy.com/v1/gifs/search",
+    dataType: "json",
+    data: $("form").serialize(),
+    success: successCallback,
+    error: errorCallback,
+  })
+}
+function successCallback(response){
+   response.data.forEach(function(element,i){
+     $(".gif-gallery").append($("<img src="+ element.images.fixed_height.url +">"));
+   });
+}
+function errorCallback(){
+  console.log("error");
+}
